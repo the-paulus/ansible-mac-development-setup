9d87ab605e7a08ad9c2943bdcc36f2597d5d287e
Paul Lyon <pmlyon@gmail.com>
2018-05-30 23:24:06 -0500

    Installing virtualbox and ansible via brew.

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Enter your password to install ansible using pip."
-sudo -s --  "easy_install pip && pip install ansible docker-py"
-echo "Enter your password again to continue running the setup."
+brew install ansible
+brew cask install virtualbox virtualbox-extension-pack
 ansible-playbook setup.yml -e "username=\"$(id -u -nr)\" group=\"$(id -g -nr)\" homedir=$HOME" --ask-sudo-pass
-
